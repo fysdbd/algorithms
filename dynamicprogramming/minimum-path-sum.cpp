133470b4c3c971a65ee96e33e476005fe6f8c1fc
fysdbd <fysdbd@gmail.com>
2017-01-31 15:14:20 +0800

    dynamic programming : minimum path sum

--- dynamicprogramming/minimum-path-sum.cpp
+++ dynamicprogramming/minimum-path-sum.cpp
@@ -2,36 +2,40 @@
 /*
     Minimum Path Sum
 
-    description(leetcode
-                https://leetcode.com/problems/minimum-path-sum/
+    description(lintcode:
+                http://www.lintcode.com/en/problem/minimum-path-sum/
                 )
 
     Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
 
-    Note: You can only move either down or right at any point in time.
-
+    You can only move either down or right at any point in time.
 
     notes:
 */
 
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        
+    /**
+     * @param grid: a list of lists of integers.
+     * @return: An integer, minimizes the sum of all numbers along its path
+     */
+    typedef vector<int> row;
+    typedef vector<row> matirx;
+    int minPathSum(vector<vector<int> > &grid) {
         if (grid.empty() || grid[0].empty()) { return 0; }
-        int m = grid.size(), n = grid.at(0).size();
-        vector<int> sum_matrix(m*n);
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) { sum_matrix[0] = grid[0][0]; continue; }
-                auto left = (j == 0 ? INT_MAX : sum_matrix[i*n + j - 1]);
-                auto up = (i == 0 ? INT_MAX : sum_matrix[(i-1)*n + j]);
-                sum_matrix[i*n + j] = std::min(left, up) + grid[i][j];
+        int R = grid.size(), C = grid[0].size();
+        for (int r = 1; r < R; r++) { grid[r][0] += grid[r-1][0]; }
+        for (int c = 1; c < C; c++) { grid[0][c] += grid[0][c-1]; }
+        
+        for (int r = 1; r < R; r++) {
+            for (int c = 1; c < C; c++) {
+                grid[r][c] += std::min(grid[r-1][c], grid[r][c-1]);
             }
         }
-
-        return sum_matrix.back();
+        
+        return grid[R-1][C-1];
     }
 };
+
 
