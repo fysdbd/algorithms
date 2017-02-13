ca283c289bb1849f7d2bf4ac2c39eaee97a2f5c9
fysdbd <fysdbd@gmail.com>
2017-02-13 08:46:43 +0800

    best time to buy and sell stock - ii

---
+++ dynamicprogramming/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,25 @@
+
+/*
+    Best Time to Buy and Sell Stock II
+
+    Say you have an array for which the ith element is the price of a given stock on day i.
+
+    Design an algorithm to find the maximum profit.
+    You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times).
+    However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) { return 0; }
+
+        int profit = 0, last = prices[0];
+        for (auto price : prices) {
+            profit += std::max(0, (price-last));
+            last = price;
+        }
+        return profit;
+    }
+};
